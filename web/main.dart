4b8f0d33ac8143b59e9146c4e492bef41fab07c5
antonleb <antonleb@cisco.com>
2017-06-29 19:13:09 +0200

    delete doesn't work yet but now the links to mains music streaming provider works

--- web/main.dart
+++ web/main.dart
@@ -8,6 +8,8 @@ List<Album> releasesBatch;
 Timer timerRemainingTime;
 bool isEmailPaused;
 
+Element elementLastClicked;
+
 Future<String> get(String url, {Map<String, String> headers, String body}){
   Completer completer = new Completer<String>();
   HttpRequest req = new HttpRequest()..open("GET", url);
@@ -28,22 +30,24 @@ Future<String> getMethod(String method, {String body}) {
 }
 
 void main() {
-  getMethod("getWebBatch").then((String rep){
-    List<Map> batchJson = JSON.decode(rep);
-    releasesBatch = new List.generate(batchJson.length, (int idx) => new Album(batchJson[idx]));
-    display();
+  window.onClick.listen((MouseEvent event){
+    manualBlur(elementLastClicked);
+    elementLastClicked = event.target;
   });
+  getWebBatch();
   getEmailBatchInfos();
   prepareEmailsButtons();
 }
 
-void display(){
-  for (Album album in releasesBatch){
-    querySelector("#listAlbums").append(album.createDiv());
-    if (!album.isImageUrlSet){
-      album.setImageUrl();
+void getWebBatch(){
+  getMethod("getWebBatch").then((String rep){
+    List<Map> batchJson = JSON.decode(rep);
+    releasesBatch = new List.generate(batchJson.length, (int idx) => new Album(batchJson[idx]));
+    querySelector("#listAlbums").children.clear();
+    for (Album album in releasesBatch){
+      querySelector("#listAlbums").append(album.createDiv());
     }
-  }
+  });
 }
 
 void getEmailBatchInfos(){
@@ -102,6 +106,40 @@ void prepareEmailsButtons(){
   });
 }
 
+void manualBlur(Element element){
+  if (element == null) return;
+  releasesBatch.forEach((Album album){
+    if (album.imageWrapperDiv == element){
+      album.clickableWrapperDiv.style.zIndex = "";
+      album.imageDiv.style.setProperty("-webkit-filter", "");
+      album.metadataWrapperDiv.style.setProperty("-webkit-filter", "");
+    }
+  });
+}
+
+class SearchUrlHelper {
+
+  static String googlePlayMusicSearchUrl = "https://play.google.com/music/listen?u=0#/sr/";
+  static String deezerSearchUrl = "http://www.deezer.com/search/";
+  static String spotifySearchUrl = "https://open.spotify.com/search/results/";
+
+  static String searchGoogleMusic(String searchTerms){
+    List<String> toEncode = searchTerms.split(" ");
+    toEncode = toEncode.map<String>((String s) => Uri.encodeFull(s)).toList(growable: false);
+    String search = toEncode.join("+");
+    return googlePlayMusicSearchUrl + search;
+  }
+
+  static String searchDeezer(String searchTerms){
+    return deezerSearchUrl + Uri.encodeFull(searchTerms);
+  }
+  
+  static String searchSpotify(String searchTerms){
+    return spotifySearchUrl + Uri.encodeFull(searchTerms);
+  }
+
+}
+
 class Album {
 
   static String LastFM_API_KEY = "";
@@ -121,23 +159,29 @@ class Album {
   String artist;
   String mbid;
 
-  String chosenImageUrl;
+  String _chosenImageUrl;
 
   DivElement artistDiv;
+  DivElement metadataWrapperDiv;
+  DivElement imageWrapperDiv;
+  DivElement clickableWrapperDiv;
   DivElement imageDiv;
 
-  bool isImageUrlSet = false;
+  Completer _imageUrlSetCompleter;
+  Future _imageDivCreated;
 
   Album(Map json) {
+    _imageUrlSetCompleter = new Completer();
+    _imageDivCreated = _imageUrlSetCompleter.future;
     title = json["title"];
     first_release_date = DateFromString(json["first-release-date"]);
     primary_type = json["primary-type"];
     artist = json["artist"];
     mbid = json["mbid"];
     getImageUrl().then((_){
-      if (imageDiv != null) {
-        setImageUrl();
-      }
+      _imageDivCreated.then((_){
+        imageDiv.style.backgroundImage = 'url("$_chosenImageUrl")';
+      });
     });
   }
 
@@ -146,39 +190,71 @@ class Album {
     artistDiv.classes.add("artist");
     DivElement content = new DivElement();
     content.classes..add("content");
-    artistDiv..append(content..append(_metadataDiv())..append(_imageDiv()));
+    artistDiv..append(content..append(_metadataDiv())..append(_imageDiv())..append(_clickableLinksDiv()));
     return artistDiv;
   }
 
   DivElement _metadataDiv(){
-    DivElement metadataWrapperDiv = new DivElement();
+    metadataWrapperDiv = new DivElement();
     metadataWrapperDiv.classes.add("metadataWrapper");
-    DivElement nameDiv = new DivElement();
-    nameDiv.classes.add("name");
-    DivElement playCountDiv = new DivElement();
-    playCountDiv.classes.add("artistName");
-    nameDiv.text = this.title;
-    playCountDiv.text = this.artist.length > 24 ? this.artist.substring(0, 21) + "..." : this.artist;
-    return metadataWrapperDiv..append(nameDiv)..append(playCountDiv);
+    DivElement titleDiv = new DivElement();
+    titleDiv.classes.add("name");
+    DivElement artistNameDiv = new DivElement();
+    artistNameDiv.classes.add("artistName");
+    titleDiv.text = this.title.length > 24 ? this.title.substring(0, 21) + "..." : this.title;
+    artistNameDiv.text = this.artist.length > 24 ? this.artist.substring(0, 21) + "..." : this.artist;
+    return metadataWrapperDiv..append(titleDiv)..append(artistNameDiv);
   }
 
   DivElement _imageDiv(){
-    DivElement imageWrapperDiv = new DivElement();
+    imageWrapperDiv = new DivElement();
     imageWrapperDiv.classes.add("imageWrapper");
     imageDiv = new DivElement();
     imageDiv.classes.add("image");
+    _imageUrlSetCompleter.complete();
+    imageWrapperDiv.onClick.listen((_) {
+      clickableWrapperDiv.style.zIndex = "1";
+      imageDiv.style.setProperty("-webkit-filter", "blur(3px)");
+      metadataWrapperDiv.style.setProperty("-webkit-filter", "blur(1px)");
+    });
     return imageWrapperDiv..append(imageDiv);
   }
 
-  void setImageUrl(){
-    imageDiv.style.backgroundImage = 'url("$chosenImageUrl")';
-    isImageUrlSet = true;
+  DivElement _clickableLinksDiv(){
+    clickableWrapperDiv = new DivElement();
+    clickableWrapperDiv.classes.add("clickableWrapper");
+    DivElement googleMusicDiv = new DivElement();
+    googleMusicDiv.classes..add("icon")..add("googleMusic");
+    googleMusicDiv.style.backgroundImage = "url('https://play-music.gstatic.com/fe/b9659330fe8ab3a7debed69d371b7063/favicon_96x96.png')";
+    googleMusicDiv.onClick.listen((_) =>
+        window.open(SearchUrlHelper.searchGoogleMusic("$title $artist"), "Search on Google Music"));
+    DivElement deezerDiv = new DivElement();
+    deezerDiv.classes..add("icon")..add("deezer");
+    deezerDiv.style.backgroundImage = "url('https://e-cdns-files.dzcdn.net/images/common/favicon/favicon-96x96-v00400039.png')";
+    deezerDiv.onClick.listen((_) =>
+        window.open(SearchUrlHelper.searchDeezer("$title $artist"), "Search on Deezer"));
+    DivElement spotifyDiv = new DivElement();
+    spotifyDiv.classes..add("icon")..add("spotify");
+    spotifyDiv.style.backgroundImage = "url('https://play.spotify.edgekey.net/site/e244a4f/images/favicon.png')";
+    spotifyDiv.onClick.listen((_) =>
+        window.open(SearchUrlHelper.searchSpotify("$title $artist"), "Search on Spotify"));
+    DivElement deleteDiv = new DivElement();
+    deleteDiv.classes..add("icon")..add("delete");
+    deleteDiv.style.backgroundImage = "url('garbage.png')";
+    deleteDiv.onClick.listen((_){
+      List<Map<String, String>> encoding = new List()..add(this);
+      getMethod("deleteReleases", body: JSON.encode(encoding)).then((_){
+        getWebBatch();
+      });
+    });
+    clickableWrapperDiv..append(googleMusicDiv)..append(deezerDiv)..append(spotifyDiv)..append(deleteDiv);
+    return clickableWrapperDiv;
   }
 
   Future getImageUrl() async {
-    chosenImageUrl = _getCachedChosenImageUrl() ?? await _fetchChosenImageUrl();
-    if (chosenImageUrl == "")
-      chosenImageUrl = blankCoverArtUrl;
+    _chosenImageUrl = _getCachedChosenImageUrl() ?? await _fetchChosenImageUrl();
+    if (_chosenImageUrl == "")
+      _chosenImageUrl = blankCoverArtUrl;
   }
 
   String _getCachedChosenImageUrl(){
@@ -205,6 +281,16 @@ class Album {
         }
       }
     });
+  }
+
+  Map<String, String> toJson() {
+    return {
+      "title": title,
+      "first-release-date": StringFromDate(first_release_date),
+      "primary-type": primary_type,
+      "artist": artist,
+      "mbid": mbid
+    };
   }
 
 
